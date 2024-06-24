b2994d1382032a61fcc87ec0de2a5688c4028cec
sujanale10 <sujanale404@gmail.com>
2024-06-24 12:33:00 +1000

    small change in padding bottom

--- src/Components/Repairsection/repairsection.js
+++ src/Components/Repairsection/repairsection.js
@@ -7,16 +7,26 @@ export const Repairsection = () => {
   const repariPaymentContainer = useRef(null);
   const repairPaymentOptionsContainer = useRef(null);
   const repairSectionRef = useRef(null);
-  
+
   const updatePaddingBottom = () => {
-    if (repariPaymentContainer.current && repairPaymentOptionsContainer.current) {
-      const totalHeight = repariPaymentContainer.current.offsetHeight + repairPaymentOptionsContainer.current.offsetHeight;
+    if (
+      repariPaymentContainer.current &&
+      repairPaymentOptionsContainer.current
+    ) {
+      const totalHeight =
+        repariPaymentContainer.current.offsetHeight +
+        repairPaymentOptionsContainer.current.offsetHeight;
       repairSectionRef.current.style.paddingBottom = `${totalHeight}px`;
       console.log("The total height of the two containers is: ", totalHeight);
     }
   };
-  window.addEventListener('resize', updatePaddingBottom);
-  window.addEventListener('load', updatePaddingBottom);
+
+  useEffect(() => {
+    window.addEventListener("resize", updatePaddingBottom);
+    document.addEventListener("load", updatePaddingBottom);
+
+    updatePaddingBottom();
+  });
 
   return (
     <section className="repair-section" ref={repairSectionRef}>
@@ -31,7 +41,10 @@ export const Repairsection = () => {
                 <h1>LOGO</h1>
               </div>
             </Col>
-            <Col lg={6} className="repair-payment-stretch-container repair-payment-stretch-container-show ">
+            <Col
+              lg={6}
+              className="repair-payment-stretch-container repair-payment-stretch-container-show "
+            >
               <Row className="payment-description d-flex justify-content-center align-items-center">
                 <Col lg={12} className="payment-description-plan text-center">
                   <h3>Ask about our payment plans</h3>
@@ -63,43 +76,46 @@ export const Repairsection = () => {
               />
             </Col>
           </Row>
-          <div className="repair-payment-options-container d-flex justify-content-between flex-wrap" ref={repairPaymentOptionsContainer}>
+          <div
+            className="repair-payment-options-container d-flex justify-content-between flex-wrap"
+            ref={repairPaymentOptionsContainer}
+          >
             {/* <Row className="d-flex flex-wrap"> */}
-              {/* <Col lg={4}> */}
-                <div className="repair-payment-option-i d-flex justify-content-between align-items-center">
-                  <div className="repair-payment-option-i-title">
-                    <h1>01</h1>
-                  </div>
-                  <div className="repair-payment-option-i-description">
-                    <h1>Do it now</h1>
-                    <p>Big jobs or little jobs, repaired today.</p>
-                  </div>
-                  <div className="repair-payment-option-separator repair-payment-option-separator-hide"></div>
-                </div>
-              {/* </Col>
+            {/* <Col lg={4}> */}
+            <div className="repair-payment-option-i d-flex justify-content-between align-items-center">
+              <div className="repair-payment-option-i-title">
+                <h1>01</h1>
+              </div>
+              <div className="repair-payment-option-i-description">
+                <h1>Do it now</h1>
+                <p>Big jobs or little jobs, repaired today.</p>
+              </div>
+              <div className="repair-payment-option-separator repair-payment-option-separator-hide"></div>
+            </div>
+            {/* </Col>
               <Col lg={4}> */}
-                <div className="repair-payment-option-ii d-flex justify-content-between align-items-center">
-                  <div className="repair-payment-option-ii-title">
-                    <h1>02</h1>
-                  </div>
-                  <div className="repair-payment-option-ii-description">
-                    <h1>Pay later</h1>
-                    <p>Pay if off in small, affordable installments.</p>
-                  </div>
-                  <div className="repair-payment-option-separator repair-payment-option-separator-hide"></div>
-                </div>
-              {/* </Col>
+            <div className="repair-payment-option-ii d-flex justify-content-between align-items-center">
+              <div className="repair-payment-option-ii-title">
+                <h1>02</h1>
+              </div>
+              <div className="repair-payment-option-ii-description">
+                <h1>Pay later</h1>
+                <p>Pay if off in small, affordable installments.</p>
+              </div>
+              <div className="repair-payment-option-separator repair-payment-option-separator-hide"></div>
+            </div>
+            {/* </Col>
               <Col lg={4}> */}
-                <div className="repair-payment-option-iii d-flex justify-content-between align-items-center">
-                  <div className="repair-payment-option-iii-title">
-                    <h1>03</h1>
-                  </div>
-                  <div className="repair-payment-option-iii-description">
-                    <h1>No interest ever</h1>
-                    <p>Talk to us to find out more.</p>
-                  </div>
-                </div>
-              {/* </Col>
+            <div className="repair-payment-option-iii d-flex justify-content-between align-items-center">
+              <div className="repair-payment-option-iii-title">
+                <h1>03</h1>
+              </div>
+              <div className="repair-payment-option-iii-description">
+                <h1>No interest ever</h1>
+                <p>Talk to us to find out more.</p>
+              </div>
+            </div>
+            {/* </Col>
             </Row> */}
           </div>
         </div>
